cd3158ec669520b0c5e4dbcda6f788940e93f1f2
josetpm <1823963@aluold.murciaeduca.es>
2024-10-03 00:56:12 +0200

    cambio cosas de setting.py

--- judoyamato/settings.py
+++ judoyamato/settings.py
@@ -13,6 +13,11 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from decouple import config
 import os
 from pathlib import Path
+import dj_database_url
+
+
+DATABASES = {"default": dj_database_url.config(default=config("DATABASE_URL"))}
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
