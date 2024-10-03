d15f53d0ccfeb300a1279503bdd4353ba1edcbd3
josetpm <1823963@aluold.murciaeduca.es>
2024-10-03 18:18:56 +0200

    cambio settings.py

--- judoyamato/settings.py
+++ judoyamato/settings.py
@@ -129,10 +129,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = "static/"
-if not DEBUG:
-
-    STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-    STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+""""
+if not DEBUG:"""
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 
 # Default primary key field type
