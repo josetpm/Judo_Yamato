76b19eee31002eba1475c8e862c7ae12b1320dfd
josetpm <1823963@aluold.murciaeduca.es>
2024-10-03 23:29:17 +0200

    cambio en home.html

--- build.sh
+++ build.sh
@@ -1,27 +1,6 @@
-#!/bin/bash
 set -o errexit
 
-# Instalar las dependencias desde el archivo requirements.txt
-echo "Instalando dependencias..."
 pip install -r requirements.txt
 
-# Recopilar archivos estáticos sin interacción
-echo "Recopilando archivos estáticos..."
-python manage.py collectstatic --no-input
-
-# Crear migraciones
-echo "Creando migraciones..."
-python manage.py makemigrations
-
-# Aplicar migraciones a la base de datos
-echo "Aplicando migraciones..."
+python manage.py collectsatic --no-input
 python manage.py migrate
-
-# Crear un superusuario sin interacción, si no existe
-echo "Creando superusuario..."
-
-export DJANGO_SUPERUSER_PASSWORD=Pass_Word5
-
-python manage.py createsuperuser --noinput --username superuser --email judo.yamato.club@gmail.com || true
-
-echo "Proceso de construcción completado."
