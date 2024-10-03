e1640aa8f28e1a76dcfb9d0a1144e5b7bafef249
josetpm <1823963@aluold.murciaeduca.es>
2024-10-03 17:53:31 +0200

    build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,10 @@
 set -o errexit
 
-# pip install -r requirements.txt
+pip install -r requirements.txt
 
 python manage.py collectsatic --no-input
+python manage.py makemigrations
 python manage.py migrate
+
+
+python manage.py createsuperuser --noinput --username admin --email admin@example.com || true
