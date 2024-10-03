45cb66cc98f4f19c26fd616d6bc052eb58c5605e
josetpm <1823963@aluold.murciaeduca.es>
2024-10-03 18:00:42 +0200

    faltaba una t en collecstatic

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@ set -o errexit
 
 pip install -r requirements.txt
 
-python manage.py collectsatic --no-input
+python manage.py collectstatic --no-input
 python manage.py makemigrations
 python manage.py migrate
 
